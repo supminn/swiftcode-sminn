68abf63dd0571e8336d37b51badf425237f2793a
supriya <sendtosupriya@gmail.com>
2017-10-07 15:32:55 +0530

    postman changes

--- app/services/NewsAgentService.java
+++ app/services/NewsAgentService.java
@@ -13,6 +13,25 @@ import java.util.concurrent.ExecutionException;
 public class NewsAgentService {
     public NewsAgentResponse getNewsAgentResponse(String keyword,UUID sessionId)
     {
-        return null;
+        //return null;
+        NewsAgentResponse newsAgentResponse = new NewsAgentResponse();
+        try{
+            WSRequest queryRequest = WS.url("https://api.api.ai/api/query");
+            CompletionStage<WSResponse> responsePromise = queryRequest
+                    .setQueryParameter("v","20150910")
+                    .setQueryParameter("query",keyword)
+                    .setQueryParameter("lang","en")
+                    .setQueryParameter("sessionId",sessionId.toString())
+                    .setQueryParameter("timezone", "2017-10-07TO02:50:23+0530")
+                    .setHeader("Authorization","Bearer 946df4ead6524dbcaeb5c6c2409462b6")
+                    .get();
+            JsonNode response = responsePromise.thenApply(WSResponse::asJson).toCompletableFuture().get();
+            newsAgentResponse.keyword = response.get("result").get("parameters").get("keyword").asText();
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        return newsAgentResponse;
+        }
     }
-}
+
