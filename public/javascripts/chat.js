654e4db6db4d2394cc5751e737af738f9787d23d
supriya <sendtosupriya@gmail.com>
2017-10-08 10:55:04 +0530

    UI changes

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -2,4 +2,27 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 
 app.controller('chatController', function ($scope, $sce) {
+
+    $scope.messages = [
+        {
+            'sender':'USER',
+            'text':'hi',
+            'time':'9:52 AM'
+        },
+         {
+            'sender':'BOT',
+            'text':'may i help you',
+            'time':'9:53AM'
+         },
+          {
+             'sender':'USER',
+             'text':'news about Modi',
+             'time':'9:54 AM'
+          },
+           {
+              'sender':'BOT',
+              'text':'give me a moment',
+              'time':'9:55 AM'
+           }
+    ]
 });
